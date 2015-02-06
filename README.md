e231df73fdfbc91c1e923d51edb25abbb6fb47fe
Darío Javier Cravero <dario@uxtemple.com>
2015-02-06 13:32:59 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# react-live
+
+Live React editor using Ace.
+
+It also supports marty.js. Feel free to PR your framework of choice :).
+
+NOTE: Everything's raw.
+
+MIT license
+by Darío Javier Cravero
---
+++ react-live.react.js
@@ -0,0 +1,48 @@
+var ReactLive = React.createClass({
+  getInitialState: function() {
+    return {
+      editor: undefined
+    }
+  },
+  compile: function() {
+    // The "evil" call :)
+    eval(JSXTransformer.transform(this.state.editor.getSession().getValue()).code)
+  },
+  render: function() {
+    return (
+      <div id='react-live'>
+        <button id='compile' onClick={this.compile}>compile</button>
+        <div id='editor'></div>
+        <div id='el'>You can dump stuff on #el</div>
+      </div>
+    )
+  },
+  componentDidMount: function() {
+    this.state.editor = ace.edit('editor')
+    this.state.editor.setTheme('ace/theme/pastel_on_dark')
+
+    var session = this.state.editor.getSession()
+    session.setTabSize(2)
+    session.setMode('ace/mode/jsx')
+    session.setValue(
+      "var el   = document.getElementById('el')\n\n" +
+      "var View = React.createClass({\n" +
+      "  render: function() {\n" +
+      "    return (\n" +
+      "      <div>\n" +
+      "      </div>\n" +
+      "    )\n" +
+      "  }\n" +
+      "})\n\n" +
+      "React.render(\n" +
+      "  <View />,\n" +
+      "  el\n" +
+      ")"
+    )
+  }
+})
+
+React.render(
+  <ReactLive />,
+  document.getElementById('react-live-container')
+)
