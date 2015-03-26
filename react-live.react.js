62712cb348b6a319945ea5b38ab0257404b0c033
Darío Javier Cravero <dario@uxtemple.com>
2015-03-26 00:06:27 +0000

    Updated react to 0.13.1

--- react-live.react.js
+++ react-live.react.js
@@ -1,54 +1,62 @@
 /*eslint-disable no-eval*/
 /*global ace, BabelTransform, React*/
 
-const SAMPLE_CODE = `let el = document.getElementById('el');
+const SAMPLE_CODE = `let el = document.getElementById('el')
 
-let View = React.createClass({
-  render: function() {
+class View extends React.Component {
+  render() {
     return (
       <div>
         You can dump stuff on #el
       </div>
-    );
+    )
   }
-})
+}
 
 React.render(
   <View />,
   el
-);`;
+)`
 
-let ReactLive = React.createClass({
-  getInitialState: function() {
+class ReactLive extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = this.getInitialState()
+  }
+
+  getInitialState() {
     return {
       editor: undefined
-    };
-  },
-  compile: function() {
+    }
+  }
+
+  compile() {
     // The 'evil' call :)
-    eval(BabelTransform.transform(this.state.editor.getSession().getValue()).code);
-  },
-  render: function() {
+    eval(BabelTransform.transform(this.state.editor.getSession().getValue()).code)
+  }
+
+  render() {
     return (
       <div id='react-live'>
-        <button id='compile' onClick={this.compile}>compile</button>
+        <button id='compile' onClick={this.compile.bind(this)}>compile</button>
         <div id='editor'></div>
         <div id='el'>You can dump stuff on #el</div>
       </div>
-    );
-  },
-  componentDidMount: function() {
-    this.state.editor = ace.edit('editor');
-    this.state.editor.setTheme('ace/theme/pastel_on_dark');
-
-    let session = this.state.editor.getSession();
-    session.setTabSize(2);
-    session.setMode('ace/mode/jsx');
-    session.setValue(SAMPLE_CODE);
+    )
   }
-});
+
+  componentDidMount() {
+    this.state.editor = ace.edit('editor')
+    this.state.editor.setTheme('ace/theme/pastel_on_dark')
+
+    let session = this.state.editor.getSession()
+    session.setTabSize(2)
+    session.setMode('ace/mode/jsx')
+    session.setValue(SAMPLE_CODE)
+  }
+}
 
 React.render(
   <ReactLive />,
   document.getElementById('react-live-container')
-);
+)
